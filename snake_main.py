441309782e18cbe68a51c11c4f338f07d4701045
Ilenog <86084976+Ilenog@users.noreply.github.com>
2021-06-18 17:52:41 -0500

    a little editing, added project setting files

--- snake_main.py
+++ snake_main.py
@@ -7,7 +7,7 @@ from scoreboard import Score
 screen = Screen()
 border = Turtle()
 
-screen.setup(1000, 800)
+screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
@@ -15,12 +15,12 @@ screen.colormode(255)
 border.ht()
 border.color("red")
 border.penup()
-border.setpos(-495, -385)
+border.setpos(-295, -285)
 border.pendown()
-border.setpos(-495, 395)
-border.setpos(485, 395)
-border.setpos(485, -385)
-border.setpos(-495, -385)
+border.setpos(-295, 295)
+border.setpos(285, 295)
+border.setpos(285, -285)
+border.setpos(-295, -285)
 screen.listen()
 
 snake = Snake()
@@ -43,15 +43,15 @@ while new_game:
         score.ate_food()
         snake.extend()
 
-    if snake.head.xcor() > 480 or snake.head.xcor() < -490 or snake.head.ycor() > 390 or snake.head.ycor() < -380:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -280:
         new_game = False
         score.game_over()
 
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             new_game = False
             score.game_over()
+
+# TODO: add looping to ask for a new game every time the game is over
 
 screen.mainloop()
